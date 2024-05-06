16aae3a7d3350e16c33d4ab20435f2a6e37081fa
Mitchell Robert Vollger <mrvollger@gmail.com>
2024-05-06 08:18:32 -0700

    add.

--- snakemake_executor_plugin_slurm/__init__.py
+++ snakemake_executor_plugin_slurm/__init__.py
@@ -92,7 +92,7 @@ class Executor(RemoteExecutor):
         if wildcard_str == "":
             comment_str = f"rule_{job.name}"
         else:
-            comment_str = f"rule_{job.name}_wildcards_{wildcard_str}"
+            comment_str = f"rule_{job.name}__wildcards_{wildcard_str}"
         call = (
             f"sbatch --job-name {self.run_uuid} --output {slurm_logfile} --export=ALL "
             f"--comment {comment_str}"
