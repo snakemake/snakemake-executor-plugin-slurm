45f3b396249de43e062176291f245481f1769412
meesters <meesters@uni-mainz.de>
2024-08-16 14:20:18 +0200

    feat: new functionality to remove SLURM env vars prior to submitting in job context

---
+++ snakemake_executor_plugin_slurm/utils.py
@@ -0,0 +1,15 @@
+# utility functions for the SLURM executor plugin
+
+import os
+
+def delete_slurm_environment():
+    """
+    Function to delete all environment variables 
+    starting with 'SLURM_'. The parent shell, will
+    still have the this environment. Needed to
+    submit within a SLURM job context to avoid
+    conflicting environments.
+    """"
+    for var in os.environ:
+        if 'SLURM' in var:
+            os.unsetenv(var)
