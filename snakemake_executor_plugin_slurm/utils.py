c27f5f8b4dcf7c2d9bc34fd4870d13ff24c0dfae
Christian Meesters <cmeesters@users.noreply.github.com>
2024-08-23 08:47:46 +0200

    feat: in job stability (#137)
    might increase stability of in-job submissions, might fix #113 
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    
    - **New Features**
    - Introduced a utility function to manage SLURM environment variables,
    enhancing job submission reliability.
    - Updated warning handling to clean up SLURM environments when specific
    conditions are met.
    
    - **Bug Fixes**
    - Improved resource management by ensuring lingering SLURM job contexts
    are addressed.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ snakemake_executor_plugin_slurm/utils.py
@@ -0,0 +1,16 @@
+# utility functions for the SLURM executor plugin
+
+import os
+
+
+def delete_slurm_environment():
+    """
+    Function to delete all environment variables
+    starting with 'SLURM_'. The parent shell will
+    still have this environment. This is needed to
+    submit within a SLURM job context to avoid
+    conflicting environments.
+    """
+    for var in os.environ:
+        if var.startswith("SLURM_"):
+            del os.environ[var]
