b2a44de88074fed92761b73ebac83205a0c7fc25
Johannes Koester <johannes.koester@uni-due.de>
2024-09-27 16:16:02 +0200

    fix test class

--- tests/tests.py
+++ tests/tests.py
@@ -2,12 +2,19 @@ from typing import Optional
 import snakemake.common.tests
 from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
 
+from snakemake_executor_plugin_slurm import ExecutorSettings
 
-class TestWorkflowsBase(snakemake.common.tests.TestWorkflowsLocalStorageBase):
+
+class TestWorkflows(snakemake.common.tests.TestWorkflowsLocalStorageBase):
     __test__ = True
 
     def get_executor(self) -> str:
         return "slurm"
 
     def get_executor_settings(self) -> Optional[ExecutorSettingsBase]:
-        return None
+        return ExecutorSettings()
+
+
+class TestWorkflowsRequeue(TestWorkflows):
+    def get_executor_settings(self) -> Optional[ExecutorSettingsBase]:
+        return ExecutorSettings(requeue=True)
