b0ff160533536e2d82f738ad6e9e1a268ba616cb
Sophie Laube <laubesop@uni-mainz.de>
2024-09-27 16:31:12 +0200

    feat: added requeue option to client (#136)
    added option for users to indicate the sbatch requeue flag if the
    cluster configuration does no re-queuing on default
    
    this is a boolean flag. We wait for the interface_plugin to be fixed.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new option to allow requeuing of preempted or failed jobs
    in the job execution settings.
    - Added a `--requeue` flag to the job submission command for enhanced
    control over job handling.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: meesters <meesters@uni-mainz.de>
    Co-authored-by: Johannes Köster <johannes.koester@uni-due.de>
    Co-authored-by: Christian Meesters <cmeesters@users.noreply.github.com>

--- snakemake_executor_plugin_slurm/__init__.py
+++ snakemake_executor_plugin_slurm/__init__.py
@@ -42,6 +42,18 @@ class ExecutorSettings(ExecutorSettingsBase):
             "required": False,
         },
     )
+    requeue: bool = field(
+        default=False,
+        metadata={
+            "help": """
+                    Allow requeuing preempted of failed jobs,
+                    if no cluster default. Results in `sbatch ... --requeue ...`
+                    This flag has no effect, if not set.
+                    """,
+            "env_var": False,
+            "required": False,
+        },
+    )
 
 
 # Required:
@@ -79,8 +91,6 @@ class Executor(RemoteExecutor):
         self._fallback_account_arg = None
         self._fallback_partition = None
         self._preemption_warning = False  # no preemption warning has been issued
-        # providing a short-hand, even if subsequent calls seem redundant
-        self.settings: ExecutorSettings = self.workflow.executor_settings
 
     def warn_on_jobcontext(self, done=None):
         if not done:
@@ -144,6 +154,9 @@ class Executor(RemoteExecutor):
 
         call += self.get_account_arg(job)
         call += self.get_partition_arg(job)
+
+        if self.workflow.executor_settings.requeue:
+            call += " --requeue"
 
         if job.resources.get("clusters"):
             call += f" --clusters {job.resources.clusters}"
