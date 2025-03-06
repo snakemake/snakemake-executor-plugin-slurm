f89e7c51651eb34fd72694e5a52ba4eb3ba0a66c
meesters <meesters@uni-mainz.de>
2025-03-06 17:51:25 +0100

    fix: added missing import

--- tests/tests.py
+++ tests/tests.py
@@ -2,6 +2,7 @@ from typing import Optional
 import snakemake.common.tests
 from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
 from unittest.mock import MagicMock
+import pytest
 
 from snakemake_executor_plugin_slurm import ExecutorSettings
 from snakemake_executor_plugin_slurm.utils import set_gres_string
