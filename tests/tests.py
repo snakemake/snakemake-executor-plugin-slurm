22d70de920826226fc3a6673c9810f1f8bbdc958
Christian Meesters <meesters@uni-mainz.de>
2025-03-18 13:14:08 +0100

    fix: unused import

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,6 @@ from typing import Optional
 import snakemake.common.tests
 from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
 from unittest.mock import MagicMock, patch
-import sys
 import pytest
 
 from snakemake_executor_plugin_slurm import ExecutorSettings
