f98332280cd9a363ac49358d16ece57f0e5a7a03
Christian Meesters <meesters@uni-mainz.de>
2025-06-03 19:41:12 +0200

    fix: gnarf  - added pathlib import#

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,6 @@
 import os
 import re
+from pathlib import Path
 from typing import Optional
 import snakemake.common.tests
 from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
