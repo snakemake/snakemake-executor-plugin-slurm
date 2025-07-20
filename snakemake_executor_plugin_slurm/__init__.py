5e3850762c73abf1fff8ba9e1e8c18379251041f
nikostr <nikostr@users.noreply.github.com>
2025-07-20 13:05:31 +0200

    feat: treat sbatch errors as job errors instead of workflow errors (#322)
    fixes
    https://github.com/snakemake/snakemake-executor-plugin-slurm/issues/320
    When I do some very rudimentary testing it seems to work: when sbatch
    fails, it is handled like a job error rather than a workflow error.
    
    The argument for `report_job_error` needs to be a `SubmittedJobInfo`.
    These jobs are technically never reported as submitted. I'm not sure if
    that would cause some type of problem? Would this PR need some sort of
    tests included?
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **Bug Fixes**
    - Improved error handling during SLURM job submission to prevent
    unnecessary interruptions when an error occurs, ensuring smoother
    workflow execution.
    - Fixed minor formatting issue related to job status checking for more
    accurate time calculations.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Christian Meesters <cmeesters@users.noreply.github.com>

--- snakemake_executor_plugin_slurm/__init__.py
+++ snakemake_executor_plugin_slurm/__init__.py
@@ -366,9 +366,15 @@ class Executor(RemoteExecutor):
                     process.returncode, call, output=err
                 )
         except subprocess.CalledProcessError as e:
-            raise WorkflowError(
-                f"SLURM sbatch failed. The error message was {e.output}"
+            self.report_job_error(
+                SubmittedJobInfo(job),
+                msg=(
+                    "SLURM sbatch failed. "
+                    f"The error message was '{e.output.strip()}'.\n"
+                    f"    sbatch call:\n        {call}\n"
+                ),
             )
+            return
         # any other error message indicating failure?
         if "submission failed" in err:
             raise WorkflowError(
@@ -445,7 +451,7 @@ class Executor(RemoteExecutor):
 
         # We use this sacct syntax for argument 'starttime' to keep it compatible
         # with slurm < 20.11
-        sacct_starttime = f"{datetime.now() - timedelta(days = 2):%Y-%m-%dT%H:00}"
+        sacct_starttime = f"{datetime.now() - timedelta(days=2):%Y-%m-%dT%H:00}"
         # previously we had
         # f"--starttime now-2days --endtime now --name {self.run_uuid}"
         # in line 218 - once v20.11 is definitively not in use any more,
