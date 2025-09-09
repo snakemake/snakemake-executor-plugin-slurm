fea54a9a329ad28f2d78236c8853abb01007fe51
Christian Meesters <meesters@uni-mainz.de>
2025-09-09 17:24:30 +0200

    fix: black

--- snakemake_executor_plugin_slurm/__init__.py
+++ snakemake_executor_plugin_slurm/__init__.py
@@ -319,7 +319,7 @@ class Executor(RemoteExecutor):
 
         if self.job.workflow.executor_settings.qos:
             call += f" --qos={self.job.workflow.executor_settings.qos}"
-            
+
         if self.workflow.executor_settings.reservation:
             call += f" --reservation={self.workflow.executor_settings.reservation}"
 
