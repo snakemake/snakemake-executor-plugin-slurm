03aef11c321e0a4a5528302dbe6f0b9bdec5069a
meesters <meesters@uni-mainz.de>
2025-12-15 12:44:23 +0100

    fix: formatting

--- tests/test_pass_command_as_script.py
+++ tests/test_pass_command_as_script.py
@@ -3,11 +3,13 @@ import snakemake.common.tests
 from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
 from snakemake_executor_plugin_slurm import ExecutorSettings
 
+
 class TestPassCommandAsScript(snakemake.common.tests.TestWorkflowsLocalStorageBase):
     """Integration-style test that runs the real workflow on the Slurm test cluster
     and verifies the plugin can submit the job by passing the command as a script
     via stdin (pass_command_as_script=True).
     """
+
     __test__ = True
 
     def get_executor(self) -> str:
