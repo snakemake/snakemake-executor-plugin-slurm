0463fbfeb1985daa10a299db8b6d6722b60ee818
patrickaoude <aoude.patrick@gmail.com>
2026-01-08 12:02:57 -0500

    Fix quoting for SLURM partition
    Fixes TypeError when partition is an int. (TypeError: expected string or bytes-like object, got 'int' from shlex.quote())

--- snakemake_executor_plugin_slurm/__init__.py
+++ snakemake_executor_plugin_slurm/__init__.py
@@ -962,7 +962,7 @@ We leave it to SLURM to resume your job(s)"""
         if partition:
             # we have to quote the partition, because it might
             # contain build-in shell commands
-            return f" -p {shlex.quote(partition)}"
+            return f" -p {shlex.quote(str(partition))}"
         else:
             return ""
 
