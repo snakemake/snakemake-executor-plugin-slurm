614e974c0d98c18c4e863e8efaacfc623e9da915
meesters <meesters@uni-mainz.de>
2026-01-26 13:39:12 +0100

    fix: removed unused import

--- tests/tests.py
+++ tests/tests.py
@@ -7,7 +7,7 @@ from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
 from unittest.mock import MagicMock, patch
 import pytest
 from snakemake_executor_plugin_slurm import ExecutorSettings
-from snakemake_executor_plugin_slurm.utils import set_gres_string, time
+from snakemake_executor_plugin_slurm.utils import set_gres_string
 from snakemake_executor_plugin_slurm.submit_string import get_submit_command
 from snakemake_executor_plugin_slurm.validation import (
     validate_slurm_extra,
