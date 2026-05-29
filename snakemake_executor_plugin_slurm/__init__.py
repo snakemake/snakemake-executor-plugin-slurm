ad4be32333b37a17995791fe00b6d285e9b14944
meesters <meesters@uni-mainz.de>
2026-05-29 15:58:52 +0200

    fix: formatting

--- snakemake_executor_plugin_slurm/__init__.py
+++ snakemake_executor_plugin_slurm/__init__.py
@@ -1632,10 +1632,7 @@ We leave it to SLURM to resume your job(s)""")
                 if account:
                     self.logger.warning(f"Guessed SLURM account: {account}")
                     canonical_account = validate_account(f"{account}", self.logger)
-                    if (
-                        not isinstance(canonical_account, str)
-                        or not canonical_account
-                    ):
+                    if not isinstance(canonical_account, str) or not canonical_account:
                         canonical_account = str(account)
                     self._fallback_account_arg = f" -A {shlex.quote(canonical_account)}"
                 else:
