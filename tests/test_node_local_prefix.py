d2ad7090e4feee82c11c4ebc3bf2aab03f4a3285
Christian Meesters <meesters@csc.uni-frankfurt.de>
2026-09-21 15:47:09 +0200

    fix: test also considers base64 encoding

--- tests/test_node_local_prefix.py
+++ tests/test_node_local_prefix.py
@@ -1,20 +1,21 @@
+import base64
+
 from snakemake_executor_plugin_slurm.utils import encode_deferred_envvars
 
 
-def test_encode_deferred_envvars_rewrites_simple_env_vars():
-    assert (
-        encode_deferred_envvars("/localscratch/$SLURM_JOB_ID")
-        == "/localscratch/__ENV_SLURM_JOB_ID__"
-    )
-    assert (
-        encode_deferred_envvars("/localscratch/${SLURM_JOB_ID}/run")
-        == "/localscratch/__ENV_SLURM_JOB_ID__/run"
-    )
+def test_encode_deferred_envvars_roundtrips_via_base64():
+    value = "/localscratch/$SLURM_JOB_ID"
+    encoded = encode_deferred_envvars(value)
+    assert encoded == base64.urlsafe_b64encode(value.encode("utf-8")).decode("ascii")
+    assert base64.urlsafe_b64decode(encoded.encode("ascii")).decode("utf-8") == value
 
 
-def test_encode_deferred_envvars_leaves_literal_text_alone():
-    assert encode_deferred_envvars("/localscratch/job-1") == "/localscratch/job-1"
-    assert (
-        encode_deferred_envvars(r"/localscratch/\$SLURM_JOB_ID")
-        == r"/localscratch/\$SLURM_JOB_ID"
-    )
+def test_encode_deferred_envvars_leaves_no_literal_dollar_sign():
+    encoded = encode_deferred_envvars("/localscratch/${SLURM_JOB_ID}/run")
+    assert "$" not in encoded
+
+
+def test_encode_deferred_envvars_handles_plain_text():
+    value = "/localscratch/job-1"
+    encoded = encode_deferred_envvars(value)
+    assert base64.urlsafe_b64decode(encoded.encode("ascii")).decode("utf-8") == value
